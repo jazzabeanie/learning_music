ae69803ecd59a1fcd944955850a5b3ec1aa376fc
Jared Johnston <j.johnston@aims.gov.au>
2024-04-30 13:42:37 +1000

    One of each note

--- generate_guitar_notes.sh
+++ generate_guitar_notes.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 echo "Note,String" > guitar_notes.csv
-for string in {1..5}
-do
-  for note in {A..G}
+for string in {1..5};
   do
-    echo "$note,$string" >> guitar_notes.csv
-  done
+    for note in {A..G};
+      do
+        echo "$note,$string" >> guitar_notes.csv
+      done
 done
